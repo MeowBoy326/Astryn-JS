8713b7cd5413ac0fba3dd46ae284bad95a652365
ChuXuanbbll <84491544+ChuXuanbbll@users.noreply.github.com>
2023-07-24 13:57:10 +0800

    delete useless files

--- Memory/tempCodeRunnerFile.js
+++ Memory/tempCodeRunnerFile.js
@@ -1,6 +1,1 @@
-console.time("1");
-// var test = memory._hread(processHandle, 4, buffer);
-// console.timeEnd("1");
-// console.log(test);
-// // console.log(test.resultBuf.readInt32LE(0));
-// console.log(test.resultBuf.readInt32LE(0), test.resultBuf.readInt32LE(4), test.resultBuf.readInt32LE(8));
+
